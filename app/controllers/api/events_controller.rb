a7ee76c290138d2c54fe0d89b236da860cd3b8a3
Sylvia Sievers <ssievers3@gmail.com>
2016-07-06 19:10:21 -0400

    blocmetrics-6-api fixed

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,16 +1,16 @@
 class API::EventsController < ApplicationController
-
-  before_filter :set_access_control_headers
-
-  def set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = 'Content-Type'
-  end
-    
   skip_before_action :verify_authenticity_token
+  before_filter :set_access_control_headers
+  skip_before_action :authenticate_user!
+
+   def set_access_control_headers
+     headers['Access-Control-Allow-Origin'] = '*'
+     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+     headers['Access-Control-Allow-Headers'] = 'Content-Type'
+   end
 
   def create
+    Rails.logger.info ">>> create"
     registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
     if registered_application.nil?
       render json: "Unregistered application", status: :unprocessable_entity
@@ -25,6 +25,7 @@ class API::EventsController < ApplicationController
   end
 
   def preflight
+    Rails.logger.info ">>> preflight"
     head 200
   end
 
